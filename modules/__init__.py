fa1744a9d2e7e7291f038817c829b7cc752eee62
Ganjo1st <162578733+Ganjo1st@users.noreply.github.com>
2026-03-17 21:47:36 +0700

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,5 @@
+# Пустой файл или экспорты
+from .github_actions_auth import Auth9111
+from .telegram_bot_parser import TelegramRSSParser
+from .publication_api import PublicationAPI
+from .rubric_mapper import get_rubric_id
