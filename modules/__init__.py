a1019f28c67b4aee7a82fc52252c7105d84cba5d
Ganjo1st <162578733+Ganjo1st@users.noreply.github.com>
2026-03-17 21:57:18 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,19 @@
-# Пустой файл или экспорты
+"""
+Модули для 9111 Poster
+"""
+
 from .github_actions_auth import Auth9111
 from .telegram_bot_parser import TelegramRSSParser
 from .publication_api import PublicationAPI
 from .rubric_mapper import get_rubric_id
+from .cookie_manager import CookieManager
+from .proxy_manager import ProxyManager
+
+__all__ = [
+    'Auth9111',
+    'TelegramRSSParser',
+    'PublicationAPI',
+    'get_rubric_id',
+    'CookieManager',
+    'ProxyManager'
+]
