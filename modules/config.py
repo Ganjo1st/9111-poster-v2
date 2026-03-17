8a214b55c3622dbbd11787d8f0f6da43333345e5
Ganjo1st <162578733+Ganjo1st@users.noreply.github.com>
2026-03-17 16:08:49 +0700

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    # 9111
+    NINTH_EMAIL = os.getenv("NINTH_EMAIL")
+    NINTH_PASSWORD = os.getenv("NINTH_PASSWORD")
+    USER_HASH = os.getenv("USER_HASH")
+    UUK = os.getenv("UUK")
+
+    # Telegram
+    CHANNEL_ID = os.getenv("CHANNEL_ID")
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+    # Настройки публикации
+    DEFAULT_RUBRIC = os.getenv("DEFAULT_RUBRIC", "новости")
+    DEFAULT_TAGS = os.getenv("DEFAULT_TAGS", "новости, закон, право")
+    POSTS_LIMIT = int(os.getenv("POSTS_LIMIT", 3))
