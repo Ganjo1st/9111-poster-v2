8c1e64aaffdb4f21c026de69b866ee8202677b6c
Ganjo1st <162578733+Ganjo1st@users.noreply.github.com>
2026-03-18 21:55:22 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,7 +2,8 @@
 Модули для 9111 Poster
 """
 
-from .github_actions_auth import Auth9111
+from .auth import Auth9111
+from .bypass import BypassManager
 from .telegram_bot_parser import TelegramRSSParser
 from .publication_api import PublicationAPI
 from .rubric_mapper import get_rubric_id
@@ -11,6 +12,7 @@ from .proxy_manager import ProxyManager
 
 __all__ = [
     'Auth9111',
+    'BypassManager',
     'TelegramRSSParser',
     'PublicationAPI',
     'get_rubric_id',
